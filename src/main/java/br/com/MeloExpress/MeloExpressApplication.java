1218f4230462dd4c6810e10a8f01cd8600dde00e
“Leonardo <leonardonaques@outlook.com>
2023-02-04 00:30:36 -0300

    Projeto MeloExpress - Main

---
+++ src/main/java/br/com/MeloExpress/MeloExpressApplication.java
@@ -0,0 +1,13 @@
+package br.com.MeloExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeloExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeloExpressApplication.class, args);
+	}
+
+}
