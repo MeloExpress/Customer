d11b0eb41c4c43c90de2cc3ce5446acaa397ce64
“Leonardo <leonardonaques@outlook.com>
2023-03-08 08:17:53 -0300

    Alterando o CustomerDetailsFindDTO para receber apenas os dados não sensiveis.

--- src/main/java/br/com/MeloExpress/dto/CustomerDetailsFindDTO.java
+++ src/main/java/br/com/MeloExpress/dto/CustomerDetailsFindDTO.java
@@ -5,16 +5,12 @@ import br.com.MeloExpress.domain.Customer;
 public record CustomerDetailsFindDTO(
         Long customerId,
         String companyName,
-        String cnpj,
-        String stateRegistration,
         String email, String phone,
         String responsible) {
 
     public CustomerDetailsFindDTO(Customer customer){
         this(customer.getCustomerId(),
                 customer.getCompanyName(),
-                customer.getCnpj(),
-                customer.getStateRegistration(),
                 customer.getEmail(),
                 customer.getPhone(),
                 customer.getResponsible());
